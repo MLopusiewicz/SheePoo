0d2dd4d64eb23662f977096f08aad2129a1221f6
AlanC <alancebohin92@gmail.com>
2024-04-21 12:12:00 +0200

    Score Menu

--- Assets/Hobbitowo/PoolScoring/TableManager.cs
+++ Assets/Hobbitowo/PoolScoring/TableManager.cs
@@ -1,11 +1,21 @@
 using System;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 namespace Hobbitowo.PoolScoring
 {
     public class TableManager : MonoBehaviour
     {
+        [Header("References")]
+        [SerializeField] private GameObject gameOverPanel;
+        [SerializeField] private TMP_Text timeText;
+        [SerializeField] private TMP_Text scoreText;
+
+        [Header("Settings")]
+        [SerializeField] private float timeLimit = 121f;
+        [SerializeField] private int sheepsAmount;
+        
         public int score;
         private List<TableHole> _holes;
         private List<AIController> _aiControllers;
@@ -17,11 +27,26 @@ namespace Hobbitowo.PoolScoring
             _aiControllers = new List<AIController>(GetComponentsInChildren<AIController>());
             var ratio = _holes.Count == 0? 0: _aiControllers.Count / _holes.Count; 
             _holes.ForEach((h) => h.InitializeTable(this, ratio));
+            sheepsAmount = FindObjectsOfType<AIController>().Length;
+            scoreText.text = $"{score}/ {sheepsAmount}";
+        }
+
+        private void Update()
+        {
+            timeLimit -= Time.deltaTime;
+            TimeSpan timeSpan = TimeSpan.FromSeconds(timeLimit);
+            timeText.text = string.Format("{0:00}:{1:00}", timeSpan.Minutes, timeSpan.Seconds);
+
+            if (timeLimit <= 0)
+            {
+                gameOverPanel.SetActive(true);
+            }
         }
 
         public void AddScore()
         {
             score++;
+            scoreText.text = $"{score}/ {sheepsAmount}";
             //Debug.Log($"Score: {score}");
         }
     }
